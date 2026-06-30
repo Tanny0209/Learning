5337ba6688957be4d5c259a751fba26b9141e626
Tanny0209 <tanmaypawar183@gmail.com>
2026-06-30 19:13:02 +0530

    C++ Variables

--- C++/01_Basics/01_Hello.cpp
+++ C++/01_Basics/01_Hello.cpp
@@ -1,6 +1,7 @@
-#include<iostream>
+#include<iostream>  //This is header file
+using namespace std; //This is namespace used for cout and other standard functions
 
-int main() {
-    std::cout <<"Hello , World !" << std ::endl;
+int main() { //From here the execution of program starts
+    cout <<"Hello , World !";
     return 0;
-}
+};
---
+++ C++/01_Basics/02_Variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Variable declaration and initialization
+    string name = "John Doe"; // String variable
+    int age = 25; // Integer variable
+    double height = 5.9; // Double variable
+    char grade = 'A'; // Character variable
+    bool isStudent = true; // Boolean variable
+    
+
+    // Output the values of the variables
+    cout << "Name: " << name << endl;
+    cout << "Age: " << age << endl;
+    cout << "Height: " << height << endl;
+    cout << "Grade: " << grade << endl;
+    cout << "Is Student: " << (isStudent ? "Yes" : "No") << endl;
+    
+    return 0;
+}
