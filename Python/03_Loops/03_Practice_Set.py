d9351dcc6700f824e8a4d24589a641bb5f2525ad
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-03 23:23:50 +0530

    Difficult Loop questions (Python)

--- Python/03_Loops/03_Practice_Set.py
+++ Python/03_Loops/03_Practice_Set.py
@@ -27,3 +27,89 @@ if e>=33 and f>=33 and g>=33 and total_percentage>40:
     print("You are pass.")
 else:
     print("You are fail.")
+
+
+
+# print even numbers
+for i in range(1,101):
+    if i%2==0:
+        print(i)
+
+
+# print multiplication table of a number
+mul=int(input("Enter a number to print multiplication table: "))
+for i in range(1,11):
+    print(f"{mul} x {i} = {mul*i}")
+
+
+# Factorial
+num=int(input("Enter a number to find factorial: "))
+factorial=1
+for i in range (1,num+1):
+    factorial*=i
+
+print(f"Factorial of {num} is: {factorial}")
+
+
+# Count digits in a number
+number=int(input("Enter a number to count digits: "))
+count =0
+
+for i in str(number):
+    count+=1
+print(f"Count of digits in {number} is: {count}")
+
+
+# reverse a number
+rev_number=int(input("Enter a number to reverse: "))
+rev=0
+
+while rev_number>0:
+    digit=rev_number%10
+    rev=(rev*10)+digit
+    rev_number//=10
+
+print(f"Reverse of the number is: {rev}")
+
+
+# check if a number is a palindrome or not
+palindrome_number=int(input("Enter a number to check palindrome: "))
+temp=palindrome_number
+rev=0
+while temp>0:
+    digit=temp%10
+    rev=(rev*10)+digit
+    temp//=10
+
+if palindrome_number == rev:
+    print(f"{palindrome_number} is a palindrome.")
+else:
+    print(f"{palindrome_number} is not a palindrome.")
+
+
+# Find largest digit in a number
+largest_digit_number=int(input("Enter a number to find largest digit: "))
+largest_digit=0
+
+while largest_digit_nuber>0:
+    digit = largest_digit_number%10
+    if digit>largest_digit:
+        largest_digit=digit
+    largest_digit_number//=10   
+print(f"Largest digit in the number is: {largest_digit}")
+
+
+# To check if a number is an Armstrong number or not
+arms_num = int(input("Enter a number to check if it is a armstrong number: "))
+sum=0
+temp=arms_num       
+
+while temp>0:
+    digit=temp%10
+    sum+=digit**3
+    temp//=10
+
+if sum==arms_num:
+    print(f"{arms_num} is an Armstrong number.")
+else:
+    print(f"{arms_num} is not an Armstrong number.")
