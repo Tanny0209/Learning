e7ddfb3b4c159f3df399abd257d9208eab100bbd
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-04 14:59:45 +0530

    Input Output C++

---
+++ C++/03_Input_Output/01_cout.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+ int age =56;
+
+ cout << "Age is "<< age << "\n";
+
+ return 0;
+}
---
+++ C++/03_Input_Output/02_cin.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string name;
+    int age;
+
+    cout << "Enter your fullname :- \n";
+    cin.ignore();
+    getline(cin,name);
+
+    cout<<"Enter your age : ";
+    cin  >>age;
+
+    // Multiple cin 
+    double salary;
+    char grade;
+
+    cout << "\nEnter your salary and grade : ";
+    cin >> salary >> grade;
+
+    cout<< "Your name is "<<name;
+    cout << "\nYour age is "<<age;
+
+    cout << "\nYour Salary is " <<salary <<"\nYour Grade is "<<grade <<endl;
+   
+    return 0;
+}
