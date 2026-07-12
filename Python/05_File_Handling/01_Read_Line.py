18cf4d58cdb3446f7fd4c6cc733fc425926b9751
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-12 09:56:30 +0530

    File Handling Read command

---
+++ Python/05_File_Handling/01_Read_Line.py
@@ -0,0 +1,15 @@
+text_file=open("Python/05_File_Handling/001_Text.txt","r")
+data=text_file.read()
+print(data)
+
+# only first 5 char
+data5=text_file.read(5)
+print(data5)
+
+# Read Only one line
+oneline=text_file.readline()
+print(oneline)
+
+# Returns List of strings
+list=text_file.readlines()
+print(list)
