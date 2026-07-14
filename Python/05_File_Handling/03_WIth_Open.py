8e76b893b368a61ae157cbdd9ce673dcc0c70ea5
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-14 21:52:03 +0530

    Append Fille Handling

---
+++ Python/05_File_Handling/03_WIth_Open.py
@@ -0,0 +1,4 @@
+with open ("Python/05_File_Handling/00_Text.txt","r") as file :
+    data = file.read()
+
+print(data)
---
+++ Python/05_File_Handling/04_Append.py
@@ -0,0 +1,3 @@
+with open("Python/05_File_Handling/00_Text.txt","a") as file: 
+    data = file.write("\nThis is append thing")
+
