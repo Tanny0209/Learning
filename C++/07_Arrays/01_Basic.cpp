77cecf09aed360f14a72cca1835b3e37cc0c03e6
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-15 20:23:50 +0530

    Array Basics

---
+++ C++/07_Arrays/01_Basic.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int marks[5]={35,45,65,44,66};
+    cout<< marks[1]<<endl;
+    marks[1]=46;
+    cout<< marks[1]<<endl;
+    return 0;
+}
