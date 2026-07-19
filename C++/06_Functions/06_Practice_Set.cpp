23277e489997aec02494bb41042fcfc6662f5d5e
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-19 23:14:41 +0530

    Functions

--- C++/06_Functions/06_Practice_Set.cpp
+++ C++/06_Functions/06_Practice_Set.cpp
@@ -15,8 +15,44 @@ bool eo(int n)
     
 }
 
+void evensort(int arr[],int size)
+{
+    int temp[size];
+    int index=0;
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i]%2==0)
+        {
+            temp[index]=arr[i];
+            index++;
+        }        
+    }
+
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i]%2!=0)
+        {
+            temp[index]=arr[i];
+            index++;
+        }        
+    }
+    
+    cout<<"Sorted array is :- ";
+    for (int i = 0; i < size; i++)
+    {
+        cout<<temp[i]<<" ";
+    }
+    cout<<endl;
+}
+
+
 int main()
 {
 
+    int numbers[6]={1,2,3,4,5,6};
+    int size=6;
     cout<< eo(7) <<endl;
+    evensort(numbers,size);
+    return 0;
 }
