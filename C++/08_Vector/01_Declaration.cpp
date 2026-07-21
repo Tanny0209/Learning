608da9dbca84a838f72edcef754307f091d9ef35
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-21 22:19:00 +0530

    Vector declaration

---
+++ C++/08_Vector/01_Declaration.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    vector<int> first;
+
+    vector<int> second = {10,20,30};
+
+    vector<int> third(5);
+
+    vector<int> fourth(5,100);
+    
+    return 0;
+}
