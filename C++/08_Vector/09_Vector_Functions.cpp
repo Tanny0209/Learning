05072a7e5c5d5aefcc1ed6f2a9964ac7347b6049
Tanny0209 <tanmaypawar183@gmail.com>
2026-07-22 23:01:40 +0530

    22/07/2026

---
+++ C++/08_Vector/09_Vector_Functions.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> numbers = {1,2,3,4};
+
+    cout << "Before clear:" << endl;
+    cout << "Size = " << numbers.size() << endl;
+
+    numbers.clear();
+
+    cout << "After clear:" << endl;
+    cout << "Size = " << numbers.size() << endl;
+
+    numbers.push_back(100);
+
+    cout << "First element = " << numbers.front() << endl;
+
+    numbers.insert(numbers.begin(), 10);
+
+    for(int x : numbers)
+        cout << x << " ";
+
+    cout<< endl;
+    return 0;
+}
